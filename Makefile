7ce5c80d3c856ec848e7b9782f2f383b23f6ddaa
Tom Lonergan <tomlonergan91@gmail.com>
2024-12-04 01:15:27 +0000

    day 3

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	cabal run --ghc-options="-L/usr/lib/ghc-9.2.8/site-local/regex-tdfa-1.3.2.2 -dynamic"
+	cabal run --ghc-options="-L/usr/lib/ghc-9.2.8/site-local/regex-pcfe-0.95.0.0 -dynamic"
 
 format:
 	ormolu -i **/*.hs
---
+++ lib/Day3.hs
@@ -0,0 +1,31 @@
+module Day3 (run) where
+
+import Data.List (sort, subsequences)
+import Text.Regex.PCRE
+import Utils (parseFile)
+
+filePath = "input/day_3.txt"
+
+-- Part 1
+findMul :: String -> Int
+findMul s =
+  foldl (\acc [a, b] -> acc + a * b) 0 $
+    map ((\x -> map (read :: String -> Int) $ getAllTextMatches (x =~ "[0-9]+")) :: String -> [Int]) $
+      getAllTextMatches (s =~ "mul\\([0-9]+,[0-9]+\\)") -- match everything that looks like mul(a, b)
+
+-- Part 2
+findMulWithStops :: String -> Int
+findMulWithStops s =
+  findMul $ -- we can reuse the findMul function once all invalid instructions are removed
+    (concat :: [String] -> String) $
+      -- match everything between the start of the line and the first don't(),
+      -- between any do() and don't() and between the last don't() and the end of the line
+      getAllTextMatches (s =~ "(^.*?|(?<=do\\(\\))).*?(?=don't\\(\\).*?|(?=$))")
+
+run :: IO ()
+run = do
+  instructionString <- parseFile filePath id
+  putStr "Part 1: "
+  print $ findMul $ concat instructionString
+  putStr "Part 2: "
+  print $ findMulWithStops $ concat instructionString
